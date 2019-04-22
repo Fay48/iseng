8118511c5514d61726762000ac1d4ac661faf184
Fay48 <tuan2fayofficial@gmail.com>
2019-04-22 16:29:05 +0700

    Update enc.php

--- enc.php
+++ enc.php
@@ -1,7 +1,6 @@
-
 // Encrypter Tools
 // Author : HebiroteX
-echo "[1] Base64 Encoder\n[2] MD5 Encrypter\n";
+echo "[1] Base64 Encoder\n[2] MD5 Encrypter\n[3] Password Generator\n";
 echo "What do you want? : ";
 $option = fgets(STDIN);
 // Function Base64 Encode
@@ -19,5 +18,22 @@ echo "Value : ";
 $ehbct = fgets(STDIN);
 echo "Result : ";
 echo md5($ehbct);
+echo "\n \n";
+}elseif (trim($option) == "3"){
+	function passwd($panjang)
+{
+    $karakter= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%ˆ&*()';
+    $string = '';
+    for ($i = 0; $i < $panjang; $i++) {
+  $pos = rand(0, strlen($karakter)-1);
+  $string .= $karakter{$pos};
+    }
+    return $string;
+}
+echo "=================| Password Generator |=================\n \n";
+echo "Length : ";
+$ehbct = fgets(STDIN);
+echo "Result : ";
+print passwd($ehbct);
 echo "\n \n";
 }
