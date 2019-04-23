649f3775c7eb4e31563a0b926b007d83605cb742
Fay48 <tuan2fayofficial@gmail.com>
2019-04-23 18:15:27 +0700

    SPAM MESSAGE TELEGRAM

---
+++ bot.php
@@ -0,0 +1,42 @@
+<?php
+// CHAT ID is YOUR TARGET
+// TEXT is MESSAGE FOR YOUR TARGET
+// 
+echo "
+  _    _      _     _           _      __   __
+ | |  | |    | |   (_)         | |     \ \ / /
+ | |__| | ___| |__  _ _ __ ___ | |_ ___ \ V / 
+ |  __  |/ _ \ '_ \| | '__/ _ \| __/ _ \ > <  
+ | |  | |  __/ |_) | | | | (_) | ||  __// . \ 
+ |_|  |_|\___|_.__/|_|_|  \___/ \__\___/_/ \_\ 
+                                              
+";
+echo "Do you want running this script? (y/n) : ";
+$ehbct = fgets(STDIN);
+echo "\n\n";
+if(trim($ehbct) == "y"){
+echo "Spam Count : ";
+$ehbctya = fgets(STDIN);
+echo "\n";
+echo "Running....\n";
+	$i=1;
+while ($i <= $ehbctya)
+{
+
+$curl = curl_init();
+    curl_setopt ($curl, CURLOPT_URL, "https://api.telegram.org/botYOURAPI/sendMessage?chat_id=YOURCHATID&text=YOURMESSAGES"); // Your BOT Telegram API
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+ 
+    $result = curl_exec ($curl);
+    if($result){
+    	echo "Ok ".$i."\n";
+    	$i=$i+1;
+    }else {
+    	echo "Fail Gan ".$i."\n";
+    }
+
+   }
+ //   curl_close ($curl);
+ //   print $result;
+}
+?>
